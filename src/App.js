3c5af913937f35560c1b783b378cfd4a1e4a008e
Ravi Kumar Pal <ravi.proudindian@gmail.com>
2021-01-29 10:58:55 +0530

    React hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const initalName = {
+    firstName: "",
+    lastName: "",
+  };
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState(initalName);
+  const [items, setItem] = useState([]);
+
+  const AddItem = () => {
+    setItem([
+      ...items,
+      { id: items.length, value: Math.floor(Math.random() * 10) + 1 },
+    ]);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setCount((prev) => prev + 1)}>
+        Counter {count}
+      </button>
+      <input
+        type="text"
+        value={name.firstName}
+        onChange={(e) => setName({ ...name, firstName: e.target.value })}
+      ></input>
+      <input
+        type="text"
+        value={name.lastName}
+        onChange={(e) => setName({ ...name, lastName: e.target.value })}
+      ></input>
+      <p>
+        First Name is : {name.firstName} And last name is : {name.lastName}
+      </p>
+
+      <hr></hr>
+      <button onClick={AddItem}>Add Item</button>
+      <ul>
+        {items.map((item) => (
+          <li>{item.value}</li>
+        ))}
+      </ul>
     </div>
   );
 }
